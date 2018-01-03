4e7ca8f4e08132dfd8284ae8cc6dc6faf62d40e2
michaeltangcn <48977743@qq.com>
2018-01-03 17:58:51 +0800

    Update ForegroundService.java

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -32,7 +32,7 @@ import android.content.res.Resources;
 import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
-import android.os.PowerManager;
+//import android.os.PowerManager;
 
 import org.json.JSONObject;
 
@@ -116,13 +116,13 @@ public class ForegroundService extends Service {
             startForeground(NOTIFICATION_ID, makeNotification());
         }
 
-        PowerManager pm = (PowerManager)
+        /*PowerManager pm = (PowerManager)
                 getSystemService(POWER_SERVICE);
 
         wakeLock = pm.newWakeLock(
                 PARTIAL_WAKE_LOCK, "BackgroundMode");
 
-        wakeLock.acquire();
+        wakeLock.acquire();*/
     }
 
     /**
@@ -132,10 +132,10 @@ public class ForegroundService extends Service {
         stopForeground(true);
         getNotificationManager().cancel(NOTIFICATION_ID);
 
-        if (wakeLock != null) {
+        /*if (wakeLock != null) {
             wakeLock.release();
             wakeLock = null;
-        }
+        }*/
     }
 
     /**
