99deb97fa116348bbda7e9ddab93885992469323
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-01-23 16:28:24 +0800

    Update OnePixelReceiver.java

--- src/android/OnePixelReceiver.java
+++ src/android/OnePixelReceiver.java
@@ -15,14 +15,14 @@ public class OnePixelReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {    //屏幕关闭启动1像素Activity
-            Toast.makeText(context,"OnePixelReceiver:屏幕关闭启动1像素Activity ", Toast.LENGTH_SHORT).show();
+//             Toast.makeText(context,"OnePixelReceiver:屏幕关闭启动1像素Activity ", Toast.LENGTH_SHORT).show();
             Log.e("OnePixelReceiver","屏幕关闭启动1像素Activity");
             Intent it = new Intent(context, OnePiexlActivity.class);
             it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(it);
         } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {   //屏幕打开 结束1像素
             Log.e("OnePixelReceiver","屏幕打开结束1像素");
-            Toast.makeText(context,"OnePixelReceiver:屏幕打开结束1像素", Toast.LENGTH_SHORT).show();
+//             Toast.makeText(context,"OnePixelReceiver:屏幕打开结束1像素", Toast.LENGTH_SHORT).show();
             context.sendBroadcast(new Intent("finish"));
             Intent main = new Intent(Intent.ACTION_MAIN);
             main.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
