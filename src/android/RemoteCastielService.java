a67a2a98f8d4a9c150cdd2655483a0e1aa5ae53e
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-01-26 19:09:51 +0800

    Update RemoteCastielService.java

--- src/android/RemoteCastielService.java
+++ src/android/RemoteCastielService.java
@@ -73,16 +73,12 @@ public class RemoteCastielService extends Service {
     public void showNotification(Context context,int startId) {
         Log.e("RemoteCastielService", "显示一个普通的通知");
         Notification notification = new NotificationCompat.Builder(context)
-                /**设置通知左边的大图标**/
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
-                /**设置通知右边的小图标**/
-                .setSmallIcon(R.mipmap.ic_launcher)
-                /**通知首次出现在通知栏，带上升动画效果的**/
+               /**通知首次出现在通知栏，带上升动画效果的**/
                 .setTicker("远程服务")
                 /**设置通知的标题**/
                 .setContentTitle("守护服务")
                 /**设置通知的内容**/
-                .setContentText("点击也跳转到MaintActivity")
+                .setContentText("...")
                 /**通知产生的时间，会在通知信息里显示**/
                 .setWhen(System.currentTimeMillis())
                 /**设置该通知优先级**/
@@ -93,12 +89,7 @@ public class RemoteCastielService extends Service {
                 .setOngoing(false)
                 /**向通知添加声音、闪灯和振动效果的最简单、最一致的方式是使用当前的用户默认设置，使用defaults属性，可以组合：**/
                 .setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND)
-                .setContentIntent(PendingIntent.getActivity(context, 1, new Intent(context, MainActivity.class), PendingIntent.FLAG_CANCEL_CURRENT))
                 .build();
-//        NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
-//        /**发起通知**/
-//        notificationManager.notify(0, notification);
-
         Log.e("RemoteCastielService","notifyId"+String.valueOf(GRAY_SERVICE_ID));
         startForeground(GRAY_SERVICE_ID, notification);
 
