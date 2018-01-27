404f67446b485b0134f699644e7ebe417b8a89c0
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-01-27 15:23:39 +0800

    Update LocalCastielService.java

--- src/android/LocalCastielService.java
+++ src/android/LocalCastielService.java
@@ -41,7 +41,7 @@ public class LocalCastielService extends Service {
     private Handler handler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
-            LocalCastielService.this.startService(new Intent(LocalCastielService.this, VVServer.class));
+            BackgroundMode.mActivity.startService(new Intent(BackgroundMode.mActivity, VVServer.class));
             Toast.makeText(LocalCastielService.this, "LocalCastielService: "+String.valueOf(msg.what)+ errorStr, Toast.LENGTH_SHORT).show();
             return true;
         }
