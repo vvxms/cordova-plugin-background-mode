ac1433a697462e0954d5d087dfbd75e227acc543
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-02-06 00:18:20 +0800

    Update MyJobService.java

--- src/android/MyJobService.java
+++ src/android/MyJobService.java
@@ -82,9 +82,9 @@ public class MyJobService extends JobService {
                Intent i = new Intent(getApplicationContext(), VVServer.class);
                startService(i);
                Log.e("MyJobService", "开始启动服务");
-               Toast.makeText(MyJobService.this, "启动服务VVServer", Toast.LENGTH_LONG).show();
+//                Toast.makeText(MyJobService.this, "启动服务VVServer", Toast.LENGTH_LONG).show();
             }else {
-                Toast.makeText(MyJobService.this, "VVServer-服务已启动", Toast.LENGTH_LONG).show();
+//                 Toast.makeText(MyJobService.this, "VVServer-服务已启动", Toast.LENGTH_LONG).show();
                 Log.e("MyJobService", "服务已启动");
             }
             
