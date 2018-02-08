48d3a94f6689052a95792276d511549a5e665909
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-02-08 18:00:01 +0800

    Update BackgroundExt.java

--- src/android/BackgroundExt.java
+++ src/android/BackgroundExt.java
@@ -290,17 +290,17 @@ class BackgroundExt {
         app.runOnUiThread(new Runnable() {
             public void run() {              
                 window.addFlags(
-//                         FLAG_ALLOW_LOCK_WHILE_SCREEN_ON 
-//                         FLAG_SHOW_WHEN_LOCKED |
-//                         FLAG_TURN_SCREEN_ON |
-//                         FLAG_DISMISS_KEYGUARD
+                        FLAG_ALLOW_LOCK_WHILE_SCREEN_ON 
+                        FLAG_SHOW_WHEN_LOCKED |
+                        FLAG_TURN_SCREEN_ON |
+                        FLAG_DISMISS_KEYGUARD
                     
                         
-                          FLAG_ALLOW_LOCK_WHILE_SCREEN_ON 
-                          |FLAG_SHOW_WHEN_LOCKED
-                          |FLAG_TURN_SCREEN_ON
-                          |FLAG_DISMISS_KEYGUARD
-                          |FLAG_KEEP_SCREEN_ON
+//                           FLAG_ALLOW_LOCK_WHILE_SCREEN_ON 
+//                           |FLAG_SHOW_WHEN_LOCKED
+//                           |FLAG_TURN_SCREEN_ON
+//                           |FLAG_DISMISS_KEYGUARD
+//                           |FLAG_KEEP_SCREEN_ON
                 );
             }
         });
