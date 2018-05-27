b46a2506132ceb1c12cf94129f52a4d72be37331
michaeltangcn <48977743@qq.com>
2018-05-27 23:03:54 +0800

    Update BackgroundMode.java

--- src/android/BackgroundMode.java
+++ src/android/BackgroundMode.java
@@ -193,11 +193,12 @@ public class BackgroundMode extends CordovaPlugin {
         }
         
         if(action.equalsIgnoreCase("GetLog")){              
-            SharedPreferences sharedPreferences = this.getSharedPreferences("TimeFile", MODE_PRIVATE);
-            if (sharedPreferences != null) {
-                String log = sharedPreferences.getString("Log","");   
-                callback.success(log);
-            }             
+            //SharedPreferences sharedPreferences = this.getSharedPreferences("TimeFile", MODE_PRIVATE);
+            //if (sharedPreferences != null) {
+              //  String log = sharedPreferences.getString("Log","");   
+                //callback.success(log);
+            //}        
+            callback.success("log");
             return true;
         } 
         
