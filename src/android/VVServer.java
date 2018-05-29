b69c0f2b12aebdf985830dbb927397bcc9288912
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-05-29 17:10:56 +0800

    Update VVServer.java

--- src/android/VVServer.java
+++ src/android/VVServer.java
@@ -192,20 +192,21 @@ public class VVServer extends Service{
         {
             //if(VVServer.this.getIntent().getAction().equals(ACTION_ALARM))
             {
-                mHanler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        WriteLog(VVServer.this,"启动闹钟");
-                        Message message = new Message();
-                        message.what = 1;
-                        handler.sendMessage(message);
-                    }
-                });
+
            
             }
         }
         
-
+        mHanler.post(new Runnable() {
+            @Override
+            public void run() {
+                WriteLog(VVServer.this,"启动闹钟");
+                Message message = new Message();
+                message.what = 1;
+                handler.sendMessage(message);
+            }
+        });
+        
         
         return START_STICKY;
     }
