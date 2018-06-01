258b78ce588ee5f94b22ae456acc058df6b44583
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-06-01 18:16:11 +0800

    Add files via upload

---
+++ src/android/NotificationUtils.java
@@ -0,0 +1,191 @@
+package com.yimasoft.android8servicetest;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+/**
+ * Created by loi on 2018/5/31.
+ */
+
+public class NotificationUtils {
+    public static NotificationManager mNotificationManager = null;
+    public static NotificationChannel mNotificationChannel = null;
+    public static NotificationCompat.Builder mNotificationBuilder = null;
+    private static Notification mNotification = null;
+    public static PendingIntent mPendingIntent = null;//非紧急意图，可设置可不设置
+    public static Intent mintent = null;
+
+    public static RemoteViews bigContentView;
+
+
+    public static String channelNameDefault = "Vv小秘书_default";//"通知渠道名称";//渠道名字
+    public static String channelIdDefault = "my_package_channel_1"; // 渠道ID
+    public static String descriptionDefault = "Vv小秘书通知渠道_default"; // 渠道解释说明
+
+    public static String channelNameOne = "Vv小秘书";//"通知渠道名称";//渠道名字
+    public static String channelIdOne = "my_package_channel_Default"; // 渠道ID
+    public static String description = "Vv小秘书通知渠道"; // 渠道解释说明
+
+    public static Notification init(Context context,int icon,String activityName){
+        if (mNotificationManager == null) {
+            mNotificationManager =  (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        //设置默认意图
+        try {
+            mintent = new Intent(context, Class.forName(activityName));
+            mintent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP| Intent.FLAG_ACTIVITY_NEW_TASK);
+            mPendingIntent = PendingIntent.getActivity(context, 0, mintent, 0);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance_default = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel cNotificationChannel = new NotificationChannel(channelIdDefault, channelNameDefault, importance_default);
+            cNotificationChannel.setDescription(descriptionDefault);
+            cNotificationChannel.enableLights(false); //是否在桌面icon右上角展示小红点
+            mNotificationManager.createNotificationChannel(cNotificationChannel);
+
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            if (mNotificationChannel == null) {
+                mNotificationChannel = new NotificationChannel(channelIdOne, channelNameOne, importance);
+                mNotificationChannel.setDescription(description);
+                mNotificationChannel.enableLights(true); //是否在桌面icon右上角展示小红点
+                mNotificationManager.createNotificationChannel(mNotificationChannel);
+            }
+            mNotificationBuilder = new NotificationCompat.Builder(context);
+            mNotificationBuilder
+                    .setSmallIcon(icon)
+                    .setContentTitle("Vv小秘书")
+                    .setContentText("Vv小秘书正在后台运行")
+                    .setContentIntent(mPendingIntent)
+                    .setPriority(Notification.PRIORITY_MAX)
+                    .setChannelId(channelIdDefault)//关键!!!!!!!!!!!
+                    .setAutoCancel(true);
+        }else{
+            mNotificationBuilder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(icon)
+                    .setContentTitle("Vv小秘书")
+                    .setContentText("Vv小秘书正在后台运行")
+                    .setPriority(Notification.PRIORITY_MAX)
+                    .setAutoCancel(true);
+        }
+        mNotification = mNotificationBuilder.build();
+
+        //自定义bigContentView
+        if (Build.VERSION.SDK_INT >= 16) {
+            bigContentView = new RemoteViews(context.getPackageName(), R.layout.remote_layout);
+//                    - setTextViewText(viewId, text)                     设置文本
+//                    - setTextColor(viewId, color)                       设置文本颜色
+//                    - setTextViewTextSize(viewId, units, size)          设置文本大小
+//                    - setImageViewBitmap(viewId, bitmap)                设置图片
+//                    - setImageViewResource(viewId, srcId)               根据图片资源设置图片
+//                    - setViewPadding(viewId, left, top, right, bottom)  设置Padding间距
+//                    - setOnClickPendingIntent(viewId, mPendingIntent)    设置点击事件
+            bigContentView.setOnClickPendingIntent(R.id.button, mPendingIntent);
+            mNotification.bigContentView = bigContentView;
+        }
+        return mNotification;
+    }
+
+
+    /**
+     * 根据图片资源设置图片
+     * @param args1 首行文字
+     * @param args2 次行文字
+     */
+    public static void upDataNotificationText(String  args1,String args2){
+        if(bigContentView!=null&&mNotificationManager!=null){
+            if(args1!=null)
+                bigContentView.setTextViewText(R.id.textView_1, args1);
+            if(args2!=null)
+                bigContentView.setTextViewText(R.id.textView_2, args2);
+            if(args1!=null||args2!=null)
+                mNotificationManager.notify(1, mNotification);
+        }
+    }
+
+    /**
+     *  更改
+     * @param id 要更改的图标的Id
+     * @param resId 资源文件的Id
+     */
+    public static void upDataNotificationIcon(int  id,int resId){
+        if(bigContentView!=null&&mNotificationManager!=null){
+                bigContentView.setImageViewResource(id, resId);
+//            bigContentView.setIcon();
+                mNotificationManager.notify(1, mNotification);
+        }
+    }
+
+
+
+    /**
+     *  设置通知栏按钮的意图
+     * @param pendingIntent 意图
+     */
+    public static void setButtonIntent(PendingIntent pendingIntent){
+        bigContentView.setOnClickPendingIntent(R.id.button, pendingIntent);
+        mNotificationManager.notify(1, mNotification);
+    }
+
+    /**
+     * 发送一个通知
+     * @param context
+     * @param icon 图标 不可为空！！！
+     * @param title 标题
+     * @param content 内容
+     * @param innerContent 子内容
+     * @param notificationId 通知的ID
+     * @param pendingIntent 点击这个通知的意图
+     */
+    public static void sendNotification(Context context,int importance,int icon, String title, String content,String innerContent, int notificationId,PendingIntent pendingIntent){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){//8.0及以上系统
+            mNotificationChannel = mNotificationManager.getNotificationChannel(channelIdOne);
+            mNotificationChannel.setImportance(importance);
+            if(mNotificationChannel.getImportance() == NotificationManager.IMPORTANCE_NONE){
+                Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+                intent.putExtra(Settings.EXTRA_APP_PACKAGE,context.getPackageName());
+                intent.putExtra(Settings.EXTRA_CHANNEL_ID, mNotificationChannel.getId());
+                context.startActivity(intent);//跳转到打开通知界面
+                Toast.makeText(context,"请手动将通知打开!",Toast.LENGTH_SHORT).show();
+            }
+            mNotificationBuilder = new NotificationCompat.Builder(context,channelIdOne);
+        }else {//8.0以下系统
+            mNotificationBuilder = new NotificationCompat.Builder(context);
+        }
+
+        if(mNotificationBuilder!=null){
+            mNotificationBuilder.setContentIntent(pendingIntent);
+        }
+        if(title!=null){
+            mNotificationBuilder.setContentTitle(title);
+        }
+        if(content!=null){
+            mNotificationBuilder.setContentText(content);
+        }
+        if(innerContent!=null){
+            mNotificationBuilder.setSubText(innerContent);
+        }
+        if(icon == -1){
+            return;
+        }
+        mNotificationBuilder
+                .setSmallIcon(icon)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setAutoCancel(true)//设置可取消
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE);//设置默认的呼吸灯和震动;
+        Notification notification = mNotificationBuilder.build();
+        mNotificationManager.notify(notificationId,notification);
+    }
+}
