79de934b07d3de54346caa9872433fc41fa28e98
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-06-09 11:42:29 +0800

    Update BackgroundMode.java

--- src/android/BackgroundMode.java
+++ src/android/BackgroundMode.java
@@ -114,7 +114,7 @@ public class BackgroundMode extends CordovaPlugin {
         if(isOpenDebugModel)
             VVServer.WriteLog(cordova.getActivity(), " initialize");
         
-        isIgnoreBatteryOption(mActivity);
+        isIgnoreBatteryOption(mActivity);//是否忽略电池优化
     }
     
     @Override
@@ -274,6 +274,31 @@ public class BackgroundMode extends CordovaPlugin {
                 VVServer.WriteLog(cordova.getActivity(), "设定的秒数(毫秒)  " + String.valueOf(time) + "\n存储的时间 " + new Date(setTime).toString()+"\n");
             }
             return true;
+        }
+        
+        if(action.equals("joinQQChatPage")){//加入到一个QQ聊天界面
+            try {
+                //跳转到添加好友，如果qq号是好友了，直接聊天
+                String url = "mqqwpa://im/chat?chat_type=wpa&uin=" + args.getString(0);//uin是发送过去的qq号码
+                mActivity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(mActivity,"请检查是否安装QQ",Toast.LENGTH_SHORT).show();
+            }      
+            return true;
+        }
+        
+        if(action.equals("joinQQGroupPage")){
+            Intent intent = new Intent();
+            intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + args.getString(0)));
+            // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            try {
+                mActivity.startActivity(intent);
+                return true;
+            } catch (Exception e) {
+                // 未安装手Q或安装的版本不支持
+                return false;
+            }
         }
         
         if(action.equals("sendNotification")){
